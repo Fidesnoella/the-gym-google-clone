ce6177d52a4a7ac055163308ed5fa480b1b27dc3
IRAGENA Fides Noella <fniragena@gmail.com>
2022-10-18 11:43:29 +0200

    add color to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     extend: {
       fontFamily: {
         "Arial": "arial,sans-serif",
+      },
+      colors: {
+        'skyblue': '#4285f4',
+        'secondary': '#1a0dab',
+        'lightdark': '#3c4043',
+        'lightgray': '#dadce0',
+        'gray': '#f8f9fa',
+        'darkgray': '#9aa0a6',
+        'lightviolet': ' #dfe1e5',
+        'white': '#f2f2f2', 
+        'zinc': '#70757a', 
       }
     },
   },
